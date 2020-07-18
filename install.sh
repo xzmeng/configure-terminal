48f496eceed2dededc2fb06fad63c1d147ed7f4f
Meng Xiangzhuo <xiangzhuo.meng@cloudchef.io>
2020-07-18 21:25:55 +0800

    自动安装fzf

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ sed -i 's/^plugins=.*/plugins=(git z zsh-autosuggestions docker)/' ~/.zshrc
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --all
 
 # install vim
 yum -y install vim
